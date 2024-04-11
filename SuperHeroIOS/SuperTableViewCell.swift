8669ba5e9bb32e05d356ea49104613865e016ee6
mananas <jesus.esteban@outlook.com>
2024-04-11 14:00:02 +0200

    2 CW 11/04/2024

---
+++ SuperHeroIOS/SuperTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SuperTableViewCell.swift
+//  SuperHeroIOS
+//
+//  Created by Mañanas on 11/4/24.
+//
+
+import UIKit
+
+class SuperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
