93fb13dba107cc324c3232dc9bf5e0cddebb6b14
mananas <jesus.esteban@outlook.com>
2024-04-16 09:47:32 +0200

    1 commit SW 16/04/2024

--- SuperHeroIOS/SecondViewController.swift
+++ SuperHeroIOS/SecondViewController.swift
@@ -13,6 +13,18 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var imageSuper: UIImageView!
     @IBOutlet weak var nomSuper: UILabel!
+    @IBOutlet weak var combat: UITextField!
+    @IBOutlet weak var durab: UITextField!
+    @IBOutlet weak var intelig: UITextField!
+    @IBOutlet weak var power: UITextField!
+    @IBOutlet weak var speed: UITextField!
+    @IBOutlet weak var strength: UITextField!
+    @IBOutlet weak var eyecol: UITextField!
+    @IBOutlet weak var gender: UITextField!
+    @IBOutlet weak var haircolor: UITextField!
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var race: UITextField!
+    @IBOutlet weak var weight: UITextField!
     
     
     override func viewDidLoad() {
@@ -20,6 +32,18 @@ class SecondViewController: UIViewController {
         
         imageSuper.loadImage(fromURL: superParam!.image.url)
         nomSuper.text=superParam?.name
+        combat.text=superParam?.powerstats.combat
+        durab.text=superParam?.powerstats.durability
+        intelig.text=superParam?.powerstats.intelligence
+        power.text=superParam?.powerstats.power
+        speed.text=superParam?.powerstats.speed
+        strength.text=superParam?.powerstats.strength
+        eyecol.text=superParam?.appearance.eyeColor
+        gender.text=superParam?.appearance.gender
+        haircolor.text=superParam?.appearance.hairColor
+        height.text=superParam?.appearance.height[1]
+        race.text=superParam?.appearance.race
+        weight.text=superParam?.appearance.weight[1]
         
     }
     
